f783de84b2d9343d49d1d2bd7c5b8b8e7262c65d
Katarzyna Krawczyk <ka.krawczyk95@gmail.com>
2021-06-20 04:58:33 +0200

    1# small refactoring of js

--- src/tools/Hamburger.js
+++ src/tools/Hamburger.js
@@ -1,55 +1,51 @@
+export default class Hamburger {
+  contructor(section, elementsArr, elementClass, iconClass) {
+    this.section = document.querySelector(`.${section}`);
+    this.hamburger = this.section.querySelector(".hamburger");
+    this.hambPopup = this.section.querySelector(".js__popup");
+    this.hambList = this.hambPopup.querySelector(".js__popup-list");
+
+    this.elementsArr = elementsArr;
+
+    this.generatePopup(elementClass, iconClass);
+    this.addListeners();
 
 
 
-export default class Hamburger{
-  contructor(section, elementsArr,elementClass,iconClass){
-    this.section= document.querySelector(`.${section}`);
-    this.hamburger=this.section.querySelector(".hamburger");
-    this.hambPopup=this.section.querySelector(".js__popup");
-    this.hambList=this.hambPopup.querySelector(".js__popup-list");
-   
-    this.elementsArr=elementsArr;
-    
-    this.generatePopup(elementClass,iconClass)
-    this.addListeners()
-    
-    
-    
-    
-   
+
+
   }
-  addListeners(){
-    this.hamburger.addEventListener("click",()=>{
-      this.hamburger.classList.toggle("active")
-      this.hambPopup.classList.toggle("active")
+  addListeners() {
+    this.hamburger.addEventListener("click", () => {
+      this.hamburger.classList.toggle("active");
+      this.hambPopup.classList.toggle("active");
     })
-    
+
   }
-generatePopup(elementClass,iconClass){
-     this.elementsArr.forEach(element=>{
-       const li = document.createElement("li")
-       li.classList.add(elementClass)
-       const span = document.createElement("span")
-       span.classList.add(`${elementClass}-text`)
-      //  span.textContent=`${this.elementsArr[index]}`
-       span.textContent=element
-       
-       const div = document.createElement("div")
-       div.classList.add(`${elementClass}-icon`)
-       const icon= document.createElement("i")
-       icon.classList.add(iconClass)
-       div.appendChild(icon);
-       
-       li.appendChild(span)
-       li.appendChild(div)
-       this.hambList.appendChild(li)
-        
-      
-     })
-     
-    }
- 
-  
+  generateElement(element) {
+    return document.createElement(element)
+  }
+  generatePopup(elementClass, iconClass) {
+    this.elementsArr.forEach(element => {
+      const li = this.generateElement("li");
+      const span = this.generateElement("span")
+      const div = this.generateElement("div");
+      const icon = this.generateElement("i");
+      li.classList.add(elementClass);
+      span.classList.add(`${elementClass}-text`);
+      span.textContent = element
+      div.classList.add(`${elementClass}-icon`);
+      icon.classList.add(iconClass);
+      div.appendChild(icon);
+      li.appendChild(span);
+      li.appendChild(div);
+      this.hambList.appendChild(li);
+
+
+    })
+
+  }
+
+
+
 }
-
-

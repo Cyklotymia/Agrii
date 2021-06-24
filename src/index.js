bf9047c9e3a80aac971094ed63e2b7e3be386721
Katarzyna Krawczyk <ka.krawczyk95@gmail.com>
2021-06-25 01:20:01 +0200

    #1 main.js in one function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './sass/main.scss';
 import Hamburger from './tools/Hamburger.js'
 import hambList from './tools/variables'
 import DynamicMenu from './tools/menuPopups.js'
+const start=()=>{
+  
 
 const hamburger = new Hamburger();
 let hambClass="hamburger"
@@ -11,6 +13,11 @@ const nav = new DynamicMenu()
 let containerClassforDynamicMenu="menu"
 let buttonsClassForDynamicMenu= "js__dynamicMenu"
 let nameOfDataForDynamicMenu="data-menu"
-nav.contructor(containerClassforDynamicMenu, buttonsClassForDynamicMenu,nameOfDataForDynamicMenu)
+nav.contructor(containerClassforDynamicMenu, buttonsClassForDynamicMenu,nameOfDataForDynamicMenu)  
+}
+
 
 // "products","company","our_brands"
+
+
+window.addEventListener('DOMContentLoaded',start)

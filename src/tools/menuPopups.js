07ac7b965afd40a960367d75db43b0e9bdf11e20
Katarzyna Krawczyk <ka.krawczyk95@gmail.com>
2021-06-24 05:14:15 +0200

    #1 make menuPopups.js more pretty -add useless ;-

--- src/tools/menuPopups.js
+++ src/tools/menuPopups.js
@@ -1,46 +1,37 @@
 export default class DynamicMenu {
     contructor(rootClass, buttonsClass, dataName) {
-        this.container = document.querySelector(`.${rootClass}`)
-        this.buttonsClass = buttonsClass
-        this.buttons = this.container.querySelectorAll(`.${this.buttonsClass}`)
-        this.dataName = dataName
-        this.arrOfAllElementsWithThisData = this.container.querySelectorAll(`[${dataName}]`)
-
-        this.addListeners()
-    }
+        this.container = document.querySelector(`.${rootClass}`);
+        this.buttonsClass = buttonsClass;
+        this.buttons = this.container.querySelectorAll(`.${this.buttonsClass}`);
+        this.dataName = dataName;
+        this.arrOfAllElementsWithThisData = this.container.querySelectorAll(`[${dataName}]`);
+        this.addListeners();
+    };
     addListeners = () => {
         document.addEventListener("click", (e) => {
             if (!e.target.closest(`[${this.dataName}]`)) {
-                this.RemoveActive()
-            }
-        })
-
+                this.RemoveActive();
+            };
+        });
         this.buttons.forEach(button => {
-
             button.addEventListener("click", (e) => {
-                const nameOfDataset = e.target.closest(`[${this.dataName}]`).getAttribute(`${this.dataName}`);
-                const allElementsWithSameDatasetName = this.container.querySelectorAll(`[${this.dataName}=${nameOfDataset}]`)
-
-                
+                const valueOfDataset = e.target.closest(`[${this.dataName}]`).getAttribute(`${this.dataName}`);
+                const allElementsWithSameDatasetName = this.container.querySelectorAll(`[${this.dataName}=${valueOfDataset}]`);
                 if (e.target.closest(`.${this.buttonsClass}`).classList.contains(`active`)) {
-                    this.RemoveActive()
-                }else{
-                    this.RemoveActive()
-                   allElementsWithSameDatasetName.forEach(elementWithSameDatasetName => {
-                    elementWithSameDatasetName.classList.add("active")
+                    this.RemoveActive();
+                } else {
+                    this.RemoveActive();
+                    allElementsWithSameDatasetName.forEach(elementWithSameDatasetName => {
+                        elementWithSameDatasetName.classList.add("active");
 
-                }) 
-                }
-               
-
-
-            })
-        })
-    }
+                    });
+                };
+            });
+        });
+    };
     RemoveActive = () => {
         this.arrOfAllElementsWithThisData.forEach(elementWithSameData => {
-            elementWithSameData.classList.remove("active")
-        })
-    }
-
-}
+            elementWithSameData.classList.remove("active");
+        });
+    };
+};

83be3e3a480db1998e1a9f236fdf1df21097bd90
Katarzyna Krawczyk <ka.krawczyk95@gmail.com>
2021-07-12 12:46:21 +0200

    #18 js for miniSlider

--- src/tools/MiniSlider.js
+++ src/tools/MiniSlider.js
@@ -1,140 +1,80 @@
 export default class MiniSlider {
     contruktor(rootClass) {
         this.section = document.querySelector(`.${rootClass}`)
-        this.miniSlider = this.section.querySelector(".js__miniSlider")
-        this.miniSliderElements = this.miniSlider.querySelectorAll(".js__miniSlider-element")
-        this.orderOfFirstElement = 0
-        this.itemToChange = this.miniSliderElements.length
-        this.arrowUp = this.section.querySelector(".js__miniSlider-arrowUp")
-        this.arrowDown = this.section.querySelector(".js__miniSlider-arrowDown")
-        this.isClicked = null
-        this.arr = []
-        this.listeners()
-        this.indexOfShowedPhoto = null
-        this.itemToChangeDown = null
-        this.itemToChangeUp = null
-        this.controlIndex()
+        this.sliderElements = this.section.querySelectorAll(".js__miniSlider-element")
+        this.sliderContainer = this.section.querySelector(".js__miniSlider")
         this.lighboxItems = this.section.querySelectorAll(".js__lightbox-item")
+        this.lighbox=this.section.querySelector(".js__lightbox")
+
+        this.setDataset()
+        this.listeners()
 
 
     }
     listeners = () => {
-
         this.section.addEventListener("click", (e) => {
-            if (e.target.closest(".js__miniSlider-arrowUp")) {
-                this.subIndex()
-
-            } else if (e.target.closest(".js__miniSlider-arrowDown")) {
-                this.addIndex()
-
-            }
-            // this.changeHeight(e)
-            this.logicOrder(e)
+            this.readArrow(e)
+            this.addActive(e)
         })
-
     }
-    controlIndex = () => {
-        this.miniSliderElements.forEach((miniSliderElement, index) => {
-            if (miniSliderElement.classList.contains("active")) {
-                this.indexOfShowedPhoto = index
+    readArrow = (e) => {
 
-            }
-        })
-        this.indexOfShowedPhoto = this.indexOfShowedPhoto > this.miniSliderElements.length - 1 ? 0 : this.indexOfShowedPhoto
-        this.indexOfShowedPhoto = this.indexOfShowedPhoto < 0 ? this.miniSliderElements.length - 1 : this.indexOfShowedPhoto
-
+        if (e.target.closest(".js__miniSlider-arrowUp")) {
+            console.log("up");
+            this.changeSlideUp()
+        } else if (e.target.closest(".js__miniSlider-arrowDown")) {
+            console.log("down");
+            this.changeSlideDown()
+        } else {
+            return
+        }
     }
-    subIndex = () => {
-        this.indexOfShowedPhoto -= 1
+    changeSlideUp = () => {
+        this.sliderContainer.prepend(this.sliderElements[this.sliderElements.length - 1])
+
+        this.updateSliderElements()
         this.changePhoto()
 
     }
-    addIndex = () => {
-        this.indexOfShowedPhoto += 1
+    changeSlideDown = () => {
+        this.sliderContainer.appendChild(this.sliderElements[0])
+        this.updateSliderElements()
         this.changePhoto()
 
     }
+    updateSliderElements = () => {
+        this.sliderElements = this.section.querySelectorAll(".js__miniSlider-element")
+    }
+    setDataset = () => {
+        this.sliderElements.forEach((sliderElement, index) => {
+            sliderElement.dataset.index=index
+            this.lighboxItems[index].dataset.index=index
+        })
+    }
+    addActive=(e)=>{
+        if ( e.target.closest(".js__miniSlider-element")) {
+            this.sliderElements.forEach(sliderElement=>{
+                sliderElement.classList.remove("active")
+            })
+             e.target.closest(".js__miniSlider-element").classList.add("active")
+             this.changePhoto()
+        }
+
+       
+    }
+
     changePhoto = () => {
-        this.miniSliderElements.forEach(miniSliderElement => {
-            miniSliderElement.classList.remove("active")
-        })
-        this.controlIndex()
-        this.miniSliderElements[this.indexOfShowedPhoto].classList.add("active")
-        console.log(this.miniSliderElements[this.indexOfShowedPhoto]);
-        this.changeLightBoxPhoto()
-
-    }
-    changeLightBoxPhoto = () => {
-        this.lighboxItems.forEach(lighboxItem => {
+        this.lighboxItems.forEach(lighboxItem=>{
             lighboxItem.classList.remove("active")
         })
-        this.lighboxItems[this.indexOfShowedPhoto].classList.add("active")
-    }
-    changeHeight = (e) => {
-        const item = this.miniSliderElements[this.indexOfShowedPhoto]
-        const style = getComputedStyle(item);
-        const itemTopmargin = style.marginTop;
-
-        const heightOfItem = this.miniSliderElements[this.indexOfShowedPhoto].offsetHeight + parseInt(itemTopmargin);
-
-        if (e.target.closest(".js__miniSlider-arrowUp")) {
-            this.miniSlider.style.transform = `translateY(${heightOfItem}px`
-        } else if (e.target.closest(".js__miniSlider-arrowDown")) {
-            this.miniSlider.style.transform = `translateY(${-heightOfItem}px`
-        }
-
-    }
-    logicOrder = (e) => {
-        if (!this.isClicked) {
-            this.miniSliderElements.forEach((miniSliderElement, index) => {
-                miniSliderElement.style.order = index
-                this.isClicked = true
-
-            })
-        }
-        this.miniSliderElements.forEach((miniSliderElement, index) => {
-            const order = miniSliderElement.style.order * 1
-            this.arr.push(order)
-
+        this.sliderElements.forEach(sliderElement=>{
+            
+            if (sliderElement.classList.contains("active")) {
+               const activeLightboxItem= this.lighbox.querySelector(`[data-index="${sliderElement.dataset.index}"]`)
+                activeLightboxItem.classList.add("active")
+                
+            }
+            
         })
-
-
-
-
-        if (e.target.closest(".js__miniSlider-arrowUp")) {
-            this.orderOfFirstElement -= 1
-            // this.itemToChange -= 1
-            this.itemToChange = this.arr.indexOf(Math.max.apply(null, this.arr))
-            // if (this.itemToChange < 0) {
-            //     this.itemToChange = this.miniSliderElements.length - 1
-
-            // }
-            console.log(this.itemToChange);
-            console.log(this.arr);
-            this.changeOrder()
-
-
-        } else if (e.target.closest(".js__miniSlider-arrowDown")) {
-            this.orderOfFirstElement += 1
-            this.itemToChange = this.arr.indexOf(Math.min.apply(null, this.arr))
-            // this.itemToChange += 1
-
-            // if (this.itemToChange > this.miniSliderElements.length - 1) {
-            //     this.itemToChange = 0
-
-            // }
-
-            this.changeOrder()
-
-
-
-        }
-
-
-    }
-    changeOrder = () => {
-        this.miniSliderElements[this.itemToChange].style.order = this.orderOfFirstElement
-        // this.miniSliderElements[this.itemToChange].dataset.order = this.orderOfFirstElement  
-        this.arr = []
     }
 }

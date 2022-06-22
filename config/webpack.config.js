318490dadcb65cc7f6cc6801e2e5e59732209894
Katarzyna Krawczyk <ka.krawczyk95@gmail.com>
2022-06-22 23:41:21 +0200

    changes for gh-pages v2

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: "Development",
             filename:"index.html",
-            template:"./index.html",
+            template:"./html/index.html",
             chunks:['main']
 
         }),
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -6,97 +6,98 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 
 module.exports = {
-  mode: "production",
-  entry: {
-      main: "./src/index.js",
-      contact:"./src/indexContact.js",
-      productLabel:"./src/productLabel.js"
-  },
-  output: {
-      filename: "js/[name].js",
-      path: path.resolve(__dirname, '../', "developFiles")
-  },
-
-  devServer: {
-      contentBase: path.resolve(__dirname, '../', "html")
+    mode: "production",
+    entry: {
+        main: "./src/index.js",
+        contact:"./src/indexContact.js",
+        productLabel:"./src/productLabel.js"
+    },
+    output: {
+        filename: "js/[name].js",
+        path: path.resolve(__dirname, '../', "productionFiles")
     },
 
-  plugins: [
-      new MiniCssExtractPlugin({
-          filename:'[name]-[contenthash:4].css'
-      }),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-          title: "Agrii",
-          filename:"index.html",
-          template:"./index.html",
-          chunks:['index']
+    devServer: {
+        contentBase: path.resolve(__dirname, '../', "html")
+      },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:'[name]-[contenthash:4].css'
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Development",
+            filename:"index.html",
+            template:"./html/index.html",
+            chunks:['main']
+
+        }),
+        new HtmlWebpackPlugin({
+          title: "Developmentt",
+          filename:"contact.html",
+          template:"./html/contact.html",
+          chunks:['contact']
 
       }),
       new HtmlWebpackPlugin({
-        title: "Agrii - kontakt",
-        filename:"contact.html",
-        template:"./html/contact.html",
-        chunks:['contact']
+        title: "Developmentt",
+        filename:"productLabel.html",
+        template:"./html/productLabel.html",
+        chunks:['productLabel']
 
     }),
-    new HtmlWebpackPlugin({
-      title: "Agrii - karta produktu ",
-      filename:"productLabel.html",
-      template:"./html/productLabel.html",
-      chunks:['productLabel']
-
-  }),
-      new CopyPlugin({
-        patterns: [
-          { from: "public/images", to: "images" },
-          // { from: "public/icons", to: "icons" },
-        ],
-      }),
-  ],
-  module:{
-      rules:[
-          {
-              test: /\.css$/,
-              use: [MiniCssExtractPlugin.loader, 'css-loader']
-            },
+        new CopyPlugin({
+          patterns: [
+            { from: "public/images", to: "images" },
+            // { from: "public/icons", to: "icons" },
+          ],
+        }),
+        
+    ],
+    module:{
+        rules:[
             {
-              test: /\.(sass|scss)$/,
-              use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-              test: /\.(jpg|png|svg|gif|jpeg)$/,
-              loader: 'file-loader',
-              options:{
-                  name:'[name][contenthash:4].[ext]',
-                  outputPath:'images',
-                  
-              }
-            },
-            {
-              test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-              use:[{
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+              },
+              {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+              },
+              {
+                test: /\.(jpg|png|svg|gif|jpeg)$/,
                 loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/'
+                options:{
+                    name:'[name][contenthash:4].[ext]',
+                    outputPath:'images',
+                    
                 }
-              }]
-            },
-            
-            {
-              test: /\.js$/,
-              loader: 'babel-loader',
-              exclude: /node_modules/,
-              options: {
-                presets: [
-                  ["@babel/preset-env", { useBuiltIns: 'usage', corejs: "2.0.0" }]
-                ],
-                plugins: [
-                  "@babel/plugin-proposal-class-properties"
-                ]
-              }
-            },
-      ]
-  }
+              },
+              {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use:[{
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/'
+                  }
+                }]
+              },
+              
+              {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                  presets: [
+                    ["@babel/preset-env", { useBuiltIns: 'usage', corejs: "2.0.0" }]
+                  ],
+                  plugins: [
+                    "@babel/plugin-proposal-class-properties"
+                  ]
+                }
+              },
+        ]
+    }
 }
